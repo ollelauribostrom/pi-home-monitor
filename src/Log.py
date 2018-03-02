0f064d91c08b4697ad2bfd3d5f63376e85fd1dc2
Olle Lauri Boström <ollebostr@gmail.com>
2018-03-02 16:13:21 +0100

    Adding tests for Log.py

--- src/Log.py
+++ src/Log.py
@@ -9,9 +9,6 @@ class Log:
   def add(self, subject, number):
     self.entries.insert(0, {'subject': subject, 'number': number})
 
-  def all(self, number):
-    filter(lambda entry: entry['number'] == number, self.entries)
-
   def latest(self, number):
     for entry in self.entries:
       if entry['number'] == number:
---
+++ tests/test_Log.py
@@ -0,0 +1,15 @@
+from src.Log import Log
+
+log = Log()
+
+def test_exposed_properties():
+  assert log.Failed_Authorization == 'failed_authorization'
+  assert log.Need_Authorization == 'need_authorization'
+
+def test_add():
+  log.add(log.Need_Authorization, "0701234567")
+  log.add(log.Failed_Authorization, "0701234567")
+
+def test_latest():
+  entry = log.latest("0701234567")
+  assert entry == log.Failed_Authorization
